1c48184a20567b4c0cec72cc2bb09d0494e60074
abilng <mail@abilng.in>
2015-07-29 01:37:26 -0700

    Added Pawn,Init

--- src/PGNReader.java
+++ src/PGNReader.java
@@ -1,5 +1,9 @@
 
 public class PGNReader {
+	private static final int BOARDSIZE = 8;
+
+	private static final String BLANK = " ";
+
 	String[][] board;
 
 	final static char BLACK = 'b';
@@ -17,11 +21,50 @@ public class PGNReader {
 
 
 	public PGNReader() {
-		board = new String[8][8];
+		board = new String[BOARDSIZE][BOARDSIZE];		
+		initBoard();
+
+	}
+
+	private void initBoard() {
+
+		for (int i = 0; i < BOARDSIZE; i++) {
+			for (int j = 0; j < BOARDSIZE; j++) {
+				board[i][j] = BLANK;
+			}
+		}
+		
+		
+		for (int i = 0; i < BOARDSIZE; i++) {
+			board[1][i] = ""+WHITE+PAWN;
+			board[6][i] = ""+BLACK+PAWN;
+		}
+		
+		board[0][0] = ""+WHITE+ROOK;
+		board[0][7] = ""+WHITE+ROOK;
+		board[7][0] = ""+BLACK+ROOK;
+		board[7][7] = ""+BLACK+ROOK;
+
+		board[0][1] = ""+WHITE+KNIGHT;
+		board[0][6] = ""+WHITE+KNIGHT;
+		board[7][1] = ""+BLACK+KNIGHT;
+		board[7][6] = ""+BLACK+KNIGHT;
+		
+		board[0][2] = ""+WHITE+BISHOP;
+		board[0][5] = ""+WHITE+BISHOP;
+		board[7][2] = ""+BLACK+BISHOP;
+		board[7][5] = ""+BLACK+BISHOP;
+		
+		
+		board[0][3] = ""+WHITE+KING;
+		board[7][4] = ""+BLACK+KING;
+		
+		board[0][4] = ""+WHITE+QUEEN;
+		board[7][3] = ""+BLACK+QUEEN;
 	}
 
 	public void move(String moveNotation) {
-		String[] s = moveNotation.split(" ");
+		String[] s = moveNotation.split(BLANK);
 		executeMove(s[0], WHITE);
 		executeMove(s[1], BLACK);
 	}
@@ -47,7 +90,7 @@ public class PGNReader {
 			moveRook(FinalPos, color);
 			break;
 		default:
-			moveQueen(FinalPos, color);
+			movePawn(FinalPos, color);
 		}
 	}
 
@@ -88,8 +131,48 @@ public class PGNReader {
 	}
 
 	
-	private void movePawn(String notation, char color) {
-		boolean  capture = notation.indexOf(CAPTURE) != -1;
+	private void movePawn(String finalPos, char color) {
+		boolean  capture = finalPos.indexOf(CAPTURE) != -1;
+		
+		int x= finalPos.charAt(0)-'a';
+		int y= finalPos.charAt(0)-'1';
+		
+		if(capture) {
+			
+			if(color == WHITE && board[x-1][y-1].equals(color+PAWN) ) {
+				board[x-1][y-1] = BLANK;				
+			
+			} else if (color == WHITE && board[x+1][y-1].equals(color+PAWN) ) {
+				board[x+1][y-1] = BLANK;
+				
+			} else if (color == BLACK && board[x-1][y+1].equals(color+PAWN) ) {
+				board[x-1][y+1] = BLANK;
+			
+			} else {
+				board[x+1][y+1] = BLANK;
+			
+			}
+			board[x][y] = ""+color+PAWN;
+			return;
+		}
+		
+		if(color == WHITE) {
+			for (int i = y-1; i >0 ; i--) {
+				if (board[x][i].equals(color+PAWN)) {
+					board[x+1][i] = BLANK;
+					break;
+				}
+			}
+		} else {
+			for (int i = y+1; i < BOARDSIZE ; i++) {
+				if (board[x][i].equals(color+PAWN)) {
+					board[x+1][i] = BLANK;
+					break;
+				}
+			}
+		}
+		board[x][y] = ""+color+PAWN;
+		
 	}
 	
 }
