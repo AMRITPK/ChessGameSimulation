bac13879d93e802e78f82161e4123820322b32fb
abilng <mail@abilng.in>
2015-07-28 23:32:25 -0700

    Init

---
+++ src/PGNReader.java
@@ -0,0 +1,26 @@
+/**
+ * @author test
+ *
+ */
+public class PGNReader {
+	String [][] board;
+	
+	final static char BLACK = 'B';
+	final static char WHITE = 'w';
+	
+	final static char PAWNS = 'P';	
+	final static char KING = 'K';
+	final static char QUEEN = 'Q';
+	final static char ROOK = 'R';
+	final static char BISHOP = 'B';
+	final static char KNIGHT = 'K';
+	
+	public PGNReader() {
+		board = new String[8][8];
+	}
+	
+	public void move(String moveNotation){
+		
+	}
+	
+}
